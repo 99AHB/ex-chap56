d0e96b0f1036a2b98c520de6a87ad28dbde3faf8
99AHB <122578987+99AHB@users.noreply.github.com>
2023-01-16 16:21:50 +0900

    5-2

--- chap5.py
+++ chap5.py
@@ -1,8 +1,26 @@
-#chap5-1
+#chap 5-1
 song = """when an eel grabs your arm,
 And it causes great harm
 That's - a moray"""
 song = song.replace('m', 'M')
 print(song)
+
+#chap 5-2
+questions = [
+"We don't serve strings around here. Are you a string?",
+"What is said on Father's Day in the forest?",
+"What makes the sound 'Sis! Boom! Bah!'?"
+]
+answers = [
+"An exploding sheep.",
+"No, I'm a frayed knot.",
+"'Pop!' goes the weasel."
+]
+print(f'Q:{questions[0]}')
+print(f'A:{answers[0]}')
+print(f'Q:{questions[1]}')
+print(f'A:{answers[1]}')
+print(f'Q:{questions[2]}')
+print(f'A:{answers[2]}')
 
 
