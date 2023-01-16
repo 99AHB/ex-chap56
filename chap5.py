287eb2c0bab3b0a9b7e8134789466cf24292c54f
99AHB <122578987+99AHB@users.noreply.github.com>
2023-01-16 16:46:12 +0900

    5-4,5

--- chap5.py
+++ chap5.py
@@ -2,7 +2,7 @@
 song = """when an eel grabs your arm,
 And it causes great harm
 That's - a moray"""
-song = song.replace('m', 'M')
+song = song.replace(' m', ' M')
 print(song)
 
 #chap 5-2
@@ -23,9 +23,29 @@ print(f'A:{answers[1]}')
 print(f'Q:{questions[2]}')
 print(f'A:{answers[2]}')
 
-# chap5-3
+# chap 5-3
 print("""My kitty cat likes %s
 My kitty cat likes %s,
 My kitty cat fell on his %s And now thinks he's a %s.""" % ('roast beef', 'ham', 'head', 'clam'))
+
+# chpa 5-4
+
+letter = """
+Dear {salutation} {name},
+Thank you for your letter. We are sorry that our {product} {verbed} in your
+{room}. Please note that it should never be used in a {room}, especially near any
+{animals}.
+
+    Send us your receipt and {amount} for shipping and handling. We will send you
+another {product} that, in our tests, is {procent}% less llikely to have {verbed}
+
+    Thank you for your support.
+    Sincerly
+    {spokesman}
+    {job_title}"""
+
+#chap 5-5
+print(format(letter))
+
 
 
