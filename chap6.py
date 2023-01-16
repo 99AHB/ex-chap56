8bfdf7f89d8e62e235b574b512921f68555a7daf
99AHB <122578987+99AHB@users.noreply.github.com>
2023-01-16 18:24:47 +0900

    6-4

--- chap6.py
+++ chap6.py
@@ -1,21 +1,21 @@
 # chap 6-1
-# for k in [3, 2, 1, 0]:
-#     print(k)
+for k in [3, 2, 1, 0]:
+    print(k)
 
 # chap 6-2
 
-# guess_me = 7
-# number = 1
-# while True:
-#     if number < guess_me:
-#         print('too low')
-#     elif    number == guess_me:
-#         print('found it')
-#         break
-#     elif    number > guess_me:
-#         print('oops')
-#         break
-#     number += 1
+guess_me = 7
+number = 1
+while True:
+    if number < guess_me:
+        print('too low')
+    elif    number == guess_me:
+        print('found it')
+        break
+    elif    number > guess_me:
+        print('oops')
+        break
+    number += 1
 
 #chap 6-3
 guess_me = 5
