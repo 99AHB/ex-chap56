768c7a438f02b87f46f44178d076c53015aae9a8
99AHB <122578987+99AHB@users.noreply.github.com>
2023-01-16 18:14:28 +0900

    6-3

--- chap6.py
+++ chap6.py
@@ -4,18 +4,34 @@ for k in [3, 2, 1, 0]:
 
 # chap 6-2
 
-guess_me = 7
-number = 1
-while True:
+# guess_me = 7
+# number = 1
+# while True:
+#     if number < guess_me:
+#         print('too low')
+#     elif    number == guess_me:
+#         print('found it')
+#         break
+#     elif    number > guess_me:
+#         print('oops')
+#         break
+#     number += 1
+
+#chap 6-3
+guess_me = 5
+number = 10
+for i in range(10):
     if number < guess_me:
         print('too low')
-    elif    number == guess_me:
-        print('found it')
+    elif number == guess_me:
+        print('fount it')
         break
-    elif    number > guess_me:
+    elif number > guess_me:
         print('oops')
         break
-    number += 1
+
+
+
 
 
 
